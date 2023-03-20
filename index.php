6c1533ee4d3cfc9e8aca73a3c678d0883843df68
nicola00 <nicolajobs00@gmail.com>
2023-03-20 18:00:29 +0100

    FEAT print key on table

--- index.php
+++ index.php
@@ -87,18 +87,11 @@
     <table class="table">
         <thead>
             <tr>
-                <th scope="col">#</th>
-                <?php
-                    foreach($hotels as $hotel){
-                        ?>
-                            <th>
-                                <?php
-                                   echo $hotel;
-                                ?>
-                            </th>
-                        <?php
-                    }
-                ?>
+                <th scope="col">name</th>
+                <th scope="col">description</th>
+                <th scope="col">parking</th>
+                <th scope="col">vote</th>
+                <th scope="col">distance_to_center</th>
             </tr>
         </thead>
         <tbody>
@@ -106,37 +99,39 @@
                     foreach($hotels as $hotel){
                         foreach($hotel as $key => $value)
                         ?>
+
                             <tr>
-                                <th>
+
+                                <td>
                                     <?php
                                         echo $hotel["name"];
                                     ?>
 
-                                </th>
-                                <th>
+                                </td>
+                                <td>
                                     <?php
                                         echo $hotel["description"];
                                     ?>
 
-                                </th>
-                                <th>
+                                </td>
+                                <td>
                                     <?php
                                         echo $hotel["parking"];
                                     ?>
 
-                                </th>
-                                <th>
+                                </td>
+                                <td>
                                     <?php
                                         echo $hotel["vote"];
                                     ?>
 
-                                </th>
-                                <th>
+                                </td>
+                                <td>
                                     <?php
                                         echo $hotel["distance_to_center"];
                                     ?>
 
-                                </th>
+                                </td>
                             </tr>
                         <?php
                     }
