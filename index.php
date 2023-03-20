bd2ce3816b2d0812bf3d835730b818512becef05
nicola00 <nicolajobs00@gmail.com>
2023-03-20 15:16:19 +0100

    FEAT ciclo foreach

--- index.php
+++ index.php
@@ -40,9 +40,27 @@
 
     ];
 
-
-    
+    foreach($hotels as $hotel_data){
+        foreach($hotel_data as $key => $value){
+            echo $key;
+            echo $value;
+        }
+    }
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+    </div>
+
+</body>
+</html>
 
